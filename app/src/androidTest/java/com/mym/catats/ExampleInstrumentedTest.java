dabb894552ffb59040e6fa691ef20fff5fa58350
mymhabib <85412229+mymhabib@users.noreply.github.com>
2022-08-23 23:39:06 +0700

    fix some naming error

--- app/src/androidTest/java/com/mym/catats/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/mym/catats/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.mymhabib.is5_10519166", appContext.getPackageName());
+        assertEquals("com.mym.catats", appContext.getPackageName());
     }
 }
